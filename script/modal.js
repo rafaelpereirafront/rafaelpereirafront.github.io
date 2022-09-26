526b6b87c3a05961c1ab861e9d40b286df74cdce
Rafael Pereira <rafaengcivilrj@outlook.com>
2022-09-25 22:26:09 -0300

    javascript do modal adicionado

--- script/modal.js
+++ script/modal.js
@@ -2,6 +2,8 @@ export default function initModal() {
   const modalOpen = document.querySelectorAll('.list-img');
   const closeModal = document.querySelector('[data-modal="close"]');
   const containModal = document.querySelector('[data-modal="wrapper"]');
+  const imageList = document.querySelectorAll('.list-img img');
+  const setImage = document.getElementById('imageModal');
 
   function OpenModal() {
     containModal.classList.toggle('active');
@@ -11,9 +13,23 @@ export default function initModal() {
     if (event.target === this) OpenModal(event);
   }
 
+  function srcTarget(event) {
+    let src = event.target.attributes.src.value;
+    setImage.setAttribute('src', src);
+  }
+
+  function tagImage() {
+    for (let n = 0; n < imageList.length; n++)
+      document
+        .getElementById(`image${n + 1}`)
+        .addEventListener('click', srcTarget);
+  }
+
   for (let i = 0; i < modalOpen.length; i++)
     modalOpen[i].addEventListener('click', OpenModal);
 
   closeModal.addEventListener('click', OpenModal);
   containModal.addEventListener('click', clickOutside);
+
+  tagImage();
 }
