9ccb665dfb0c3786afb751eaec561f72e313e73d
Rafael Pereira <rafaengcivilrj@outlook.com>
2022-09-30 18:19:54 -0300

    seção aboutMe adicionada

--- script/modal.js
+++ script/modal.js
@@ -2,34 +2,55 @@ export default function initModal() {
   const modalOpen = document.querySelectorAll('.list-img');
   const closeModal = document.querySelector('[data-modal="close"]');
   const containModal = document.querySelector('[data-modal="wrapper"]');
-  const imageList = document.querySelectorAll('.list-img img');
-  const setImage = document.getElementById('imageModal');
+  const getImage = document.querySelectorAll('.list-img img');
+  const getParagraph = document.querySelectorAll('.paragraph-modal');
+  const setImage = document.querySelector('[data-modal="imageModal"]');
+  const setTitle = document.querySelector('[data-modal="title"]');
+  const setParagraphModal = document.querySelector('[data-modal="paragraph"]');
 
   function OpenModal() {
     containModal.classList.toggle('active');
   }
 
-  function clickOutside(event) {
+  function ClickOutside(event) {
     if (event.target === this) OpenModal(event);
   }
 
-  function srcTarget(event) {
+  function SetAttributeImage(event) {
     let src = event.target.attributes.src.value;
+    let alt = event.target.attributes.alt.value;
     setImage.setAttribute('src', src);
+    setImage.setAttribute('alt', alt);
   }
 
-  function tagImage() {
-    for (let n = 0; n < imageList.length; n++)
+  function SetImage() {
+    for (let n = 0; n < getImage.length; n++)
       document
         .getElementById(`image${n + 1}`)
-        .addEventListener('click', srcTarget);
+        .addEventListener('click', SetAttributeImage);
+  }
+
+  function SetTitle() {
+    for (let n = 0; n < getImage.length; n++)
+      document.getElementById(`image${n + 1}`).addEventListener('click', () => {
+        let h2 = document.getElementById(`title${n + 1}`);
+        setTitle.textContent = h2.textContent;
+      });
+  }
+
+  function SetParagraph() {
+    for (let n = 0; n < getImage.length; n++)
+      document.getElementById(`image${n + 1}`).addEventListener('click', () => {
+        setParagraphModal.textContent = getParagraph[n].textContent;
+      });
   }
 
   for (let i = 0; i < modalOpen.length; i++)
     modalOpen[i].addEventListener('click', OpenModal);
-
   closeModal.addEventListener('click', OpenModal);
-  containModal.addEventListener('click', clickOutside);
+  containModal.addEventListener('click', ClickOutside);
 
-  tagImage();
+  SetImage();
+  SetTitle();
+  SetParagraph();
 }
